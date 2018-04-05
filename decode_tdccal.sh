70397b13d9a815ed1c31688818243f46d3b9f9ef
HidemitsuAsano <hasano@scphys.kyoto-u.ac.jp>
2018-04-05 14:56:21 +0900

    update

--- decode_tdccal.sh
+++ decode_tdccal.sh
@@ -1,21 +1,29 @@
 #!/bin/sh
 
 #taken in Jan.15 ,2018 before starting Run78 beam time 
-for i in {01..40}
-do 
-echo $i 
-$ANAHOME/bin/evanatko conf/Run78/analyzer.conf $i tkoout/out$i.root $HOME/Run78predata/tcal_00$i.dat
-done
+#for i in {01..40}
+#do 
+#echo $i 
+#$ANAHOME/bin/evanatko conf/Run78/analyzer.conf $i tkoout/out$i.root $HOME/Run78predata/tcal_00$i.dat
+#done
 
 #taken in Jan.26 , 2018 
-$ANAHOME/bin/evanatko conf/Run78/analyzer.conf $i tkoout/out163.root $HOME/Run78data/run78_0163.dat
-$ANAHOME/bin/evanatko conf/Run78/analyzer.conf $i tkoout/out164.root $HOME/Run78data/run78_0164.dat
-$ANAHOME/bin/evanatko conf/Run78/analyzer.conf $i tkoout/out165.root $HOME/Run78data/run78_0165.dat
+#$ANAHOME/bin/evanatko conf/Run78/analyzer.conf $i tkoout/out163.root $HOME/Run78data/run78_0163.dat
+#$ANAHOME/bin/evanatko conf/Run78/analyzer.conf $i tkoout/out164.root $HOME/Run78data/run78_0164.dat
+#$ANAHOME/bin/evanatko conf/Run78/analyzer.conf $i tkoout/out165.root $HOME/Run78data/run78_0165.dat
 
 
 #taken in Feb. 13th, 2018
-for i in {01..42}
+#for i in {01..42}
+#do 
+#echo $i 
+#$ANAHOME/bin/evanatko conf/Run78/analyzer.conf $i tkoout/out$i\_2.root $HOME/Run78middata/tcal_00$i.dat
+#done
+
+
+#taken in Feb. 25th, 2018
+for i in {1..41}
 do 
 echo $i 
-$ANAHOME/bin/evanatko conf/Run78/analyzer.conf $i tkoout/out$i_2.root $HOME/Run78middata/tcal_00$i.dat
+$ANAHOME/bin/evanatko conf/Run78/analyzer.conf $i tkoout/out$i\_3.root $HOME/Run78enddata/tcal_$i.dat
 done
