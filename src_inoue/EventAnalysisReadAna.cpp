516b964861ba2f54a750c4c797162f944216beee
HidemitsuAsano <hasano@scphys.kyoto-u.ac.jp>
2018-06-14 08:50:22 +0900

    update

--- src_inoue/EventAnalysisReadAna.cpp
+++ src_inoue/EventAnalysisReadAna.cpp
@@ -4,6 +4,46 @@ using namespace std;
 
 bool EventAnalysisReadAna::UAna()
 {
+  //event by event QA 
+  //beam info.
+  int nBHD = blMan->nBHD();
+  Tools::H1("h1_nBHD",nBHD,20,0,20,"BHD multiplicity");
+  int nT0  = blMan->nT0();
+  Tools::H1("h1_nT0",nT0,5,0,5,"T0 multiplicity");
+  int nbeam = anaInfo->nBeam();
+  Tools::H1("h1_nbeam",nbeam,10,0,10,"nbeam");
+  int nBLC1 = anaInfo->beam(0)->nBLC1();//# of track
+  Tools::H1("h1_nBLC1",nBLC1,10,0,10,"# of BLC1 tracks");
+  int nBLC2 = anaInfo->beam(0)->nBLC2();//# of track
+  Tools::H1("h1_nBLC2",nBLC2,10,0,10,"# of BLC2 tracks");
+  int nBPC = anaInfo->beam(0)->nBPC();//# of track
+  Tools::H1("h1_nBPC",nBPC,10,0,10,"# of BPC tracks");
+  int nFDC1 = anaInfo->beam(0)->nFDC1();//# of track
+  Tools::H1("h1_nFDC1",nFDC1,10,0,10,"# of FDC1 tracks");
+  double BLC1time = anaInfo->beam(0)->BLC1time();//GetTrackTime
+  Tools::H1("h1_BLC1time",BLC1time,600,-200,400,"BLC1 track time [nsec.]");
+  double BLC2time = anaInfo->beam(0)->BLC2time();
+  Tools::H1("h1_BLC2time",BLC2time,600,-200,400,"BLC2 track time [nsec.]");
+  double BPCtime = anaInfo->beam(0)->BPCtime();
+  Tools::H1("h1_BPCtime",BPCtime,600,-200,400,"BPC track time [nsec.]");
+  double BLC1chi2 = anaInfo->beam(0)->BLC1chi2();
+  Tools::H1("h1_BLC1chi2",BLC1chi2,100,0,100,"BLC1 chi2/ndf");
+  double BLC2chi2 = anaInfo->beam(0)->BLC2chi2();
+  Tools::H1("h1_BLC2chi2",BLC2chi2,100,0,100,"BLC2 chi2/ndf");
+  double BPCchi2 = anaInfo->beam(0)->BPCchi2();
+  Tools::H1("h1_BPCchi2",BPCchi2,100,0,100,"BPC chi2/ndf");
+  double t0BHDTOF = anaInfo->beam(0)->tof();//BHD - T0 TOF
+  Tools::H1("h1_T0BHDtof",t0BHDTOF,200,-100,100,"TOF (BHD - T0) [nsec.]");
+  double D5specchi2 = anaInfo->beam(0)->D5chi2();//chi2/ndf
+  Tools::H1("h1_D5chi2",D5specchi2,100,0,100,"beam reco. chi2/ndf");
+  double D5mom = anaInfo->beam(0)->D5mom();
+  Tools::H1("h1_D5mom",D5mom,100,0.8,1.2,"reco. Mom. [GeV/c]");
+  bool IsconnectD5BHD = MyAnaTools::connectD5BHD(anaInfo);
+  Tools::H1("h1_IsconnectD5BHD",IsconnectD5BHD,2,-0.5,1.5,"is connect D5BHD");
+  bool IsconnectBLC2BPC = MyAnaTools::connectBLC2BPC(anaInfo);
+  Tools::H1("h1_IsconnectBLC2BPC",IsconnectBLC2BPC,2,-0.5,1.5,"is connect BLC2BPC");
+  //endQA
+  
   if( !MyAnaTools::goodBeam(anaInfo) ) return true;
 
   if( cdsFileMatching() ){
@@ -25,10 +65,19 @@ bool EventAnalysisReadAna::UAna()
     fillHistReadKNpim(header, blMan, anaInfo);
     fillHistReadKNpip(header, blMan, anaInfo);
     fillHistReadKNkm(header, blMan, anaInfo);
+    static int first=0;
     if( MyAnaTools::targetNA()==1 ){
+      if(first==0){
+        std::cout << __FILE__ << " LHydrogen run is analyzed" << std::endl;
+        first++;
+      }
       fillHistReadKNpipi_H2(header, confMan, blMan, cdsMan, cdstrackMan, anaInfo);
     }
     if( MyAnaTools::targetNA()==2 ){
+      if(first==0){
+        std::cout << __FILE__ << " LDeuterium run is analyzed" << std::endl;
+        first++;
+      }
       fillHistReadKNpipi_D2(header, blMan, cdsMan, cdstrackMan, anaInfo);
       fillHistReadKPpimpim_D2(header, confMan, blMan, cdsMan, cdstrackMan, anaInfo);
       fillHistReadCDS_Lpim_D2(header, blMan, anaInfo);
@@ -36,6 +85,10 @@ bool EventAnalysisReadAna::UAna()
       //      fillHistReadVtxDEF(header, confMan, anaInfo, blMan, cdsMan, cdstrackMan);
     }
     else{
+      if(first==0){
+        std::cout << __FILE__ << " target is not chosen" << std::endl;
+        first++;
+      }
       //      fillHistReadKNpipi(header, blMan, cdsMan, cdstrackMan, anaInfo);
     }
   }
