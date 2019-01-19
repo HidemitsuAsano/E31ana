2fc188f2fbb4708c04e72b661568ce9baeaaacc6
HidemitsuAsano <hasano@scphys.kyoto-u.ac.jp>
2019-01-20 07:25:19 +0900

    update

--- src/IMPiSigmaUtil.h
+++ src/IMPiSigmaUtil.h
@@ -5,11 +5,35 @@
 #include "ConfMan.h"
 #include "HitMan.h"
 #include "CDSHitMan.h"
+#include "LocalTrack.h"
+#include "BeamLineHitMan.h"
+#include "BeamLineTrackMan.h"
+#include "BeamSpectrometer.h"
+#include "CDSTrackingMan.h"
 #include "IMPiSigmaAnaPar.h"
+#include <TLorentzVector.h>
 
 namespace Util
-{
+{ 
+
   bool EveSelectCDHMul(CDSHitMan *cdsman);
+  bool IsForwardCharge(BeamLineHitMan *blman);
+  int GetCDHNeighboringNHits(const std::vector <int> seg, const std::vector <int> allhit);
+  int GetNHitsCDCOuter(const TVector3 PosCDH, CDSHitMan *cdsman);
+  double AnaBeamSpec(ConfMan *confman,BeamLineTrackMan *bltrackman,const int blc1id, const int blc2id);
+  int CDSChargedAna(const bool docdcretiming,
+                    LocalTrack *bpctrack,
+                    CDSHitMan *cdsman,
+                    CDSTrackingMan *trackman,
+                    ConfMan *confman,
+                    const TLorentzVector beam,
+                    const double ctmt0, 
+                    std::vector <int> &cdhseg,
+                    std::vector <int> &pimid,
+                    std::vector <int> &pipid,
+                    std::vector <int> &kmid,
+                    std::vector <int> &protonid
+                    );
 
 };
 
