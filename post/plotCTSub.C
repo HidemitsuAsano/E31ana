02994ebab020780e992bf6b3f263dbe61919418b
HidemitsuAsano <hasano@scphys.kyoto-u.ac.jp>
2019-04-23 14:05:42 +0900

    added calibration macro

--- post/plotCTSub.C
+++ post/plotCTSub.C
@@ -10,9 +10,11 @@ void plotCTSub()
   gSystem->Load("./lib/libAll.so");
   
   ConfMan *conf = new ConfMan("/gpfs/home/had/hiasano/ana/k18ana/conf/Run78/analyzer_kwsk.conf");
-  for(int irun=100;irun<800;irun++){
+  std::ofstream fout("ctsublog");
+  for(int irun=100;irun<812;irun++){
+    //{int irun=601;
     char filename[1024];
-    sprintf(filename,"/gpfs/group/had/knucl/e15/asano/Run78/IMpisigmav74/evanaIMpisigma_0%03d.root",irun);
+    sprintf(filename,"/gpfs/group/had/knucl/e15/asano/Run78/IMpisigmav97/evanaIMpisigma_0%03d.root",irun);
     std::cout << filename << std::endl;
     TFile *fin = new TFile(filename,"READ");
     if(!fin->IsOpen()){ 
@@ -20,12 +22,14 @@ void plotCTSub()
       continue;
     }
     std::cout << "run # " << irun << std::endl;
+    fout << "run # " << irun << std::endl;
     conf->SetRunNumber(irun);
     conf->Initialize();
+    std::cout << "couter Map " << conf->GetCounterMapManager()->GetFileName().c_str() << std::endl;
     std::string filenamecds = conf->GetGainMapManager()->GetFileNameCDS();
     std::cout << "cdsfile name " << filenamecds.c_str() << std::endl;
     filenamecds.replace(0,28,"");
-    filenamecds.replace(filenamecds.size()-8,2,"34");
+    filenamecds.replace(filenamecds.size()-8,2,"35");
     std::cout << "outfile name " << filenamecds.c_str() << std::endl;
     //TCanvas *cctsub = new TCanvas("cctsub","cctsub",1800,1000);
     //cctsub->Divide(6,6);
@@ -38,8 +42,10 @@ void plotCTSub()
       if(hctsub !=NULL){
         ctsuboff = hctsub->GetMean();
         //std::cout << "icdh " << icdh+1 << " " << hctsub->GetMean() << std::endl;
+        fout << "icdh " << icdh+1 << " " << hctsub->GetMean() << std::endl;
       }else{
         //std::cout << "icdh " << icdh+1 << " " << 0 << std::endl;
+        fout << "icdh " << icdh+1 << " " << 0 << std::endl;
       }
       delete hctsub;
       int cr[2],sl[2],ch[2];//up,down
@@ -47,13 +53,15 @@ void plotCTSub()
       const int DOWN=1;
       conf->GetCounterMapManager()->GetCNA(CID_CDH,icdh+1,1,UP,cr[UP],sl[UP],ch[UP]);
       conf->GetCounterMapManager()->GetCNA(CID_CDH,icdh+1,1,DOWN,cr[DOWN],sl[DOWN],ch[DOWN]);
-      double temp[2],offs[2];
+      double temp[2]={0.,0.};
+      double offs[2]={0.,0.};
       //up 
       conf->GetGainMapManager()->GetParam( cr[UP],sl[UP],ch[UP], temp[UP], offs[UP] );
       conf->GetGainMapManager()->SetParam( cr[UP],sl[UP],ch[UP], temp[UP], offs[UP]+ctsuboff);
       //down
       conf->GetGainMapManager()->GetParam( cr[DOWN],sl[DOWN],ch[DOWN], temp[DOWN], offs[DOWN] );
       conf->GetGainMapManager()->SetParam( cr[DOWN],sl[DOWN],ch[DOWN], temp[DOWN], offs[DOWN]-ctsuboff);
+      
     }
     //delete conf;
     fin->Close();
