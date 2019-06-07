409990f715fd2d273cd6ad2f1f94e9a77d891647
Hidemitsu.Asano <hidemitsu.asano@riken.jp>
2019-06-07 16:03:19 +0900

    update

--- post/anacuts.h
+++ post/anacuts.h
@@ -18,10 +18,12 @@ namespace anacuts {
   //const double Sigmap_MAX = 1.21;
   //const double Sigmam_MIN = 1.18;
   //const double Sigmam_MAX = 1.22;
-  const double Sigmap_MIN = 1.18911-2.0*0.00540844;   
-  const double Sigmap_MAX = 1.18911+2.0*0.00540844;   
-  const double Sigmam_MIN = 1.19723-2.0*0.00601265;
-  const double Sigmam_MAX = 1.19723+2.0*0.00601265;  
+  const double Sigmap_center = 1.18911;
+  const double Sigmap_MIN = 1.18911-3.0*0.00540844;   
+  const double Sigmap_MAX = 1.18911+3.0*0.00540844;
+  const double Sigmam_center = 1.19723;
+  const double Sigmam_MIN = 1.19723-3.0*0.00601265;
+  const double Sigmam_MAX = 1.19723+3.0*0.00601265;  
   
   const double Sigmap_MIN_wide = 1.18911-5.0*0.00540844;   
   const double Sigmap_MAX_wide = 1.18911+5.0*0.00540844;   
@@ -29,15 +31,28 @@ namespace anacuts {
   const double Sigmam_MAX_wide = 1.19723+5.0*0.00601265;  
   
   //side band selection
-  const double Sigmap_sidelow_MIN = 1.18911-7.0*0.00540844;   
-  const double Sigmap_sidelow_MAX = 1.18911-5.0*0.00540844;   
-  const double Sigmap_sidehigh_MIN = 1.18911+5.0*0.00540844;   
-  const double Sigmap_sidehigh_MAX = 1.18911+7.0*0.00540844;   
-  
-  const double Sigmam_sidelow_MIN = 1.19723-7.0*0.00601265;
-  const double Sigmam_sidelow_MAX = 1.19723-5.0*0.00601265;  
-  const double Sigmam_sidehigh_MIN = 1.19723+5.0*0.00601265;
-  const double Sigmam_sidehigh_MAX = 1.19723+7.0*0.00601265;  
+  const double Sigmap_sidelow_MIN = 1.18911-6.0*0.00540844;   
+  const double Sigmap_sidelow_MAX = 1.18911-3.0*0.00540844;   
+  //const double Sigmap_sidelow_MIN = 1.18911-11.0*0.00540844;   
+  //const double Sigmap_sidelow_MAX = 1.18911-8.0*0.00540844;   
+  const double Sigmap_sidelow_center = (Sigmap_sidelow_MAX + Sigmap_sidelow_MIN)/2.0;
+  const double Sigmap_sidehigh_MIN = 1.18911+3.0*0.00540844;   
+  const double Sigmap_sidehigh_MAX = 1.18911+6.0*0.00540844;   
+  //const double Sigmap_sidehigh_MIN = 1.18911+8.0*0.00540844;   
+  //const double Sigmap_sidehigh_MAX = 1.18911+11.0*0.00540844;   
+  const double Sigmap_sidehigh_center = (Sigmap_sidehigh_MAX + Sigmap_sidehigh_MIN)/2.0;
+
+  const double Sigmam_sidelow_MIN = 1.19723-6.0*0.00601265;
+  const double Sigmam_sidelow_MAX = 1.19723-3.0*0.00601265;  
+  //const double Sigmam_sidelow_MIN = 1.19723-11.0*0.00601265;
+  //const double Sigmam_sidelow_MAX = 1.19723-8.0*0.00601265;  
+  const double Sigmam_sidelow_center = (Sigmam_sidelow_MAX + Sigmam_sidelow_MIN)/2.0;
+  const double Sigmam_sidehigh_MIN = 1.19723+3.0*0.00601265;
+  const double Sigmam_sidehigh_MAX = 1.19723+6.0*0.00601265;  
+  //const double Sigmam_sidehigh_MIN = 1.19723+8.0*0.00601265;
+  //const double Sigmam_sidehigh_MAX = 1.19723+11.0*0.00601265;  
+  const double Sigmam_sidehigh_center = (Sigmam_sidehigh_MAX + Sigmam_sidehigh_MIN)/2.0;
+
   /*
   const double Sigmap_MIN_wide = 1.18911-4.0*0.00540844;   
   const double Sigmap_MAX_wide = 1.18911+4.0*0.00540844;   
