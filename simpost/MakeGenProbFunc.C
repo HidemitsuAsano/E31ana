b4fc7b62fc96aed83a66c1b2bb1537a363148534
HidemitsuAsano <hasano@scphys.kyoto-u.ac.jp>
2019-06-16 20:14:05 +0900

    update

--- simpost/MakeGenProbFunc.C
+++ simpost/MakeGenProbFunc.C
@@ -4,11 +4,12 @@
 void MakeGenProbFunc()
 {
   //TFile *file = new TFile("simIMpisigma_nSppim_DoraAir_v45_v46_acc.root","READ");
-  TFile *file = new TFile("simIMpisigma_nSmpip_DoraAir_v45_v46_acc.root","READ");
+  //TFile *file = new TFile("simIMpisigma_nSmpip_DoraAir_v45_v46_acc.root","READ");
+  TFile *file = new TFile("simIMLpim_DoraAir_v2.root","READ");
   std::cout << "file name:" << file->GetName() << std::endl;
 
   //TH2F* React_q_IMPiSigma = (TH2F*)file->Get("React_q_IMPiSigma_Sp");
-  TH2F* React_q_IMPiSigma = (TH2F*)file->Get("React_q_IMPiSigma_Sm");
+  TH2F* React_q_IMPiSigma = (TH2F*)file->Get("React_q_IMPiSigma");
   double ntotal = React_q_IMPiSigma->Integral();
   std::cout << "total " << ntotal << std::endl;
 
@@ -26,8 +27,8 @@ void MakeGenProbFunc()
       x = React_q_IMPiSigma->GetXaxis()->GetBinCenter(ibinx);
       y = React_q_IMPiSigma->GetYaxis()->GetBinCenter(ibiny);
       int cont = React_q_IMPiSigma->GetBinContent(ibinx,ibiny);
-      z = 2.0/cont;
       if(cont>1){
+        z = 2.0/cont;
         dt->SetPoint(i,x,y,z);
         h2prob->SetBinContent(ibinx,ibiny,z);
         i++;
@@ -49,7 +50,7 @@ void MakeGenProbFunc()
 
   //TH2F* pro = (TH2F*)dt->Project("xy");
   //pro->Draw("colz");
-  TFile *fout = new TFile("probSm.root","RECREATE");
+  TFile *fout = new TFile("probLpim.root","RECREATE");
   //dt->Write();
   h2prob->Write();
 
