4b41bd8bc74d2849bace17b2f43f49dd518b3df5
HidemitsuAsano <hasano@scphys.kyoto-u.ac.jp>
2020-01-28 13:05:06 +0900

    update

--- simpost/plot_CDHdE.C
+++ simpost/plot_CDHdE.C
@@ -1,43 +1,38 @@
 void plot_CDHdE()
 {
 
-    TFile *_file0 = TFile::Open("simIMpisigma_nSmpip_v55.root");
+    TFile *_file0 = TFile::Open("simIMpisigma_nSmpip_v69.root");
     CDHdE->ProjectionY("pyiron");
     pyiron->SetLineColor(4);
     pyiron->SetXTitle("CDH dE [MeVee]");
+    pyiron->Scale(4./5.);
     pyiron->Draw("HE");
 
-    TFile *_file1 = TFile::Open("simIMpisigma_nSmpip_v54.root");
+    TFile *_file1 = TFile::Open("simIMpisigma_nSmpip_v67.root");
     CDHdE->ProjectionY("pynew");
     pynew->SetLineColor(2);
+    pynew->Scale(4./9.);
     pynew->Scale(0.95);
     pynew->Draw("HEsame");
 
 
-    TFile *_file2 = TFile::Open("simIMpisigma_nSmpip_DoraAir_v70.root");
+    TFile *_file2 = TFile::Open("simIMpisigma_nSmpip_v70.root");
     CDHdE->ProjectionY("pyair");
     pyair->SetLineColor(1);
     pyair->Draw("HEsame");
 
-    TFile *_file3 = TFile::Open("../post/evanaIMpisigma_v159.root");
+    TFile *_file3 = TFile::Open("../post/evanaIMpisigma_v162.root");
     //CDHdE_woK0_wSid_n->ProjectionY("pydata");
-    CDHdE_pippim->ProjectionY("pydata");
+    CDHdE_woK0_wSmid->ProjectionY("pydata");
     pydata->SetLineColor(5);
     double nair = pyair->Integral();
     double ndata = pydata->Integral();
     pydata->Scale(nair/ndata);
-    pydata->Scale(1.3);
+    pydata->Scale(1.08);
     //pydata->SetMarkerStyle(22);
     //pydata->SetMarkerColor(5);
-    pydata->GetXaxis()->SetLimits(0,8.8);
-    pydata->Scale(1.13);
+    //pydata->GetXaxis()->SetLimits(0,8.8);
+    pydata->Scale(1.47);
     pydata->Draw("HEsame");
-
-
-
-
-
-
-
 
 }
