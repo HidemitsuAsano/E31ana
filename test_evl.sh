c079b6a101cf7835051fc020d01368fad34d90d6
ASANO Hidemitsu <hidemitsu.asano@riken.jp>
2020-11-01 18:36:01 +0900

    update

---
+++ test_evl.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+./bin/evl conf/Run78/analyzer_kwsk.conf 601 test_evl_0601.root /group/had/knucl/e15/data/Run78/run78_0601.dat /group/had/knucl/e15/shinngo/Run78/evtracking/run78_0601_evtracking.root  
+
