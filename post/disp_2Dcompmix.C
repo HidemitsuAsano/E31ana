96b5665cd104353596a4d401283d05d26188da2a
HidemitsuAsano <hidemitsu.asano@riken.jp>
2020-11-29 01:36:41 +0900

    update

--- post/disp_2Dcompmix.C
+++ post/disp_2Dcompmix.C
@@ -69,7 +69,7 @@ void disp_2Dcompmix()
   
   //const double mixScale = 0.0496125;
  // const double mixScale = 0.0490125;
-  const double mixScale = 0.0430125;
+  const double mixScale = 0.0450125;
 
   //real data
   TCanvas *cMMnmiss_IMnpip_woK0_woSm_data_cut = new TCanvas("cMMnmiss_IMnpip_woK0_woSm_data_cut","cMMnmiss_IMnpip_woK0_woSm_data_cut",800,800);
