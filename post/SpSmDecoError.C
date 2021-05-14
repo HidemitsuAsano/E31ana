80ad7d78fbfa24102a34046d3101be72a08570b3
Hidemitsu Asano <hidemitsu.asano@riken.jp>
2021-05-15 04:37:30 +0900

    update

--- post/SpSmDecoError.C
+++ post/SpSmDecoError.C
@@ -1,3 +1,5 @@
+#include "anacuts.h"
+
 void SpSmDecoError(const int qcut=2)
 {
   TFile *f = NULL;
@@ -18,25 +20,55 @@ void SpSmDecoError(const int qcut=2)
   c1->cd(1);
   IMnpip_K0sub_woSp->Draw("HE");
   const int Ntry = 10000;
-  TH1D* IMnpip_K0sub_woSp_est = IMnpip_K0sub_woSp->Clone("IMnpip_K0sub_woSp_est");
-  TH1D* IMnpim_K0sub_woSm_est = IMnpim_K0sub_woSm->Clone("IMnpim_K0sub_woSm_est");
+  TH1D* IMnpip_K0sub_woSp_est = (TH1D*)IMnpip_K0sub_woSp->Clone("IMnpip_K0sub_woSp_est");
+  TH1D* IMnpim_K0sub_woSm_est = (TH1D*)IMnpim_K0sub_woSm->Clone("IMnpim_K0sub_woSm_est");
+  TGraph *gIMnpip_all = new TGraph();
+  TGraph *gIMnpim_all = new TGraph();
 
+  const int Spbin = IMnpip_K0sub_woSp->GetXaxis()->FindBin(anacuts::Sigmap_center);
+  const int Smbin = IMnpim_K0sub_woSm->GetXaxis()->FindBin(anacuts::Sigmam_center);
   for(int it=0;it<Ntry;it++){
+    if(it%1000==0) std::cout << "itry " << it << std::endl;
     IMnpip_K0sub_woSp_est->Reset();
     IMnpim_K0sub_woSm_est->Reset();
-    for(int ibin=0;ibin<IMnpip_K0sub_woSp->GetBbinsX();ibin++){
+    for(int ibin=0;ibin<IMnpip_K0sub_woSp->GetNbinsX();ibin++){
       double cont = IMnpip_K0sub_woSp->GetBinContent(ibin);
+      if(cont<0.0001) continue;
+      double bincent = IMnpip_K0sub_woSp->GetBinCenter(ibin);
       double err = IMnpip_K0sub_woSp->GetBinError(ibin);
       double gen = gRandom->Gaus(cont,err);
+      IMnpip_K0sub_woSp_est->SetBinContent(ibin,gen);
+      IMnpip_K0sub_woSp_est->SetBinError(ibin,err);
+      gIMnpip_all->AddPoint(bincent,gen);
     }
-
-    c1->cd(2);
-    IMnpim_K0sub_woSm->Draw("HE");
-    for(int ibin=0;ibin<IMnpim_K0sub_woSm->GetBbinsX();ibin++){
+    TSpline3 sIMnpip = new TSpline3("snpip",gIM
+    const double Splowbincen = IMnpip_K0sub_woSp->GetBinCenter(Spbin-1);
+    const double Sphighbincen = IMnpip_K0sub_woSp->GetBinCenter(Spbin+1);
+    TF1 *fSp = new TF1("fSp","pol1",Splowbincen,Sphighbincen);
+    IMnpip_K0sub_woSp_est->Fit("fSp","q","",Splowbincen,Sphighbincen);
+    IMnpip_K0sub_woSp_est->Draw("HE");
+    for(int ibin=0;ibin<IMnpim_K0sub_woSm->GetNbinsX();ibin++){
       double cont = IMnpim_K0sub_woSm->GetBinContent(ibin);
+      if(cont<0.0001) continue;
+      double bincent = IMnpip_K0sub_woSp->GetBinCenter(ibin);
       double err = IMnpim_K0sub_woSm->GetBinError(ibin);
       double gen = gRandom->Gaus(cont,err);
+      IMnpim_K0sub_woSm_est->SetBinContent(ibin,gen);
+      IMnpim_K0sub_woSm_est->SetBinError(ibin,err);
+      gIMnpim_all->AddPoint(bincent,gen);
     }
+    const double Smlowbincen = IMnpim_K0sub_woSm->GetBinCenter(Smbin-1);
+    const double Smhighbincen = IMnpim_K0sub_woSm->GetBinCenter(Smbin+1);
+    TF1 *fSm = new TF1("fSm","pol1",Smlowbincen,Smhighbincen);
+    IMnpim_K0sub_woSm_est->Fit("fSm","q","",Smlowbincen,Smhighbincen);
   }
-
+  
+  TCanvas *c2 = new TCanvas("c2","c2");
+  c2->Divide(2,1);
+  c2->cd(1);
+  gIMnpip_all->SetMarkerStyle(20);
+  gIMnpip_all->Draw("ap");
+  c2->cd(2);
+  gIMnpim_all->SetMarkerStyle(20);
+  gIMnpim_all->Draw("ap");
 }
