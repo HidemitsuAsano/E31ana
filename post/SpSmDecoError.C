6db1f4883318377b7a8d80574005af6e3763d483
Hidemitsu Asano <hidemitsu.asano@riken.jp>
2021-05-17 06:55:09 +0900

    update

--- post/SpSmDecoError.C
+++ post/SpSmDecoError.C
@@ -106,21 +106,25 @@ void SpSmDecoError(const int qcut=2)
   
   TCanvas *c3 = new TCanvas("c3","c3");
   Est_IMnpip_woSp_pol1->Draw("colz");
+  double mean_Sp_pol1 = Est_IMnpip_woSp_pol1->GetMean(2);
   double stddev_Sp_pol1 = Est_IMnpip_woSp_pol1->GetStdDev(2);
   std::cout  << Est_IMnpip_woSp_pol1->GetStdDev(2) << std::endl;
 
   TCanvas *c4 = new TCanvas("c4","c4");
   Est_IMnpip_woSp_3rd->Draw("colz");
+  double mean_Sp_3rd = Est_IMnpip_woSp_3rd->GetMean(2);
   double stddev_Sp_3rd = Est_IMnpip_woSp_3rd->GetStdDev(2);
   std::cout << Est_IMnpip_woSp_3rd->GetStdDev(2) << std::endl;
   
   TCanvas *c5 = new TCanvas("c5","c5");
   Est_IMnpim_woSm_pol1->Draw("colz");
+  double mean_Sm_pol1 = Est_IMnpim_woSm_pol1->GetMean(2);
   double stddev_Sm_pol1 = Est_IMnpim_woSm_pol1->GetStdDev(2);
   std::cout << Est_IMnpim_woSm_pol1->GetStdDev(2) << std::endl;
 
   TCanvas *c6 = new TCanvas("c6","c6");
   Est_IMnpim_woSm_3rd->Draw("colz");
+  double mean_Sm_3rd = Est_IMnpim_woSm_3rd->GetMean(2);
   double stddev_Sm_3rd = Est_IMnpim_woSm_3rd->GetStdDev(2);
   std::cout << Est_IMnpim_woSm_3rd->GetStdDev(2) << std::endl;
 
@@ -129,4 +133,71 @@ void SpSmDecoError(const int qcut=2)
 
   TCanvas *c8 = new TCanvas("c8","c8");
   IMnpim_K0sub_woSm->Draw("H");
+
+  TGraphErrors *gr_IMnpip_base = new TGraphErrors(IMnpip_K0sub_woSp);
+  TGraphErrors *gr_IMnpim_base = new TGraphErrors(IMnpim_K0sub_woSm);
+  TGraphErrors *gr_IMnpip_inter_pol1 = new TGraphErrors();
+  TGraphErrors *gr_IMnpim_inter_pol1 = new TGraphErrors();
+  TGraphErrors *gr_IMnpip_inter_3rd = new TGraphErrors();
+  TGraphErrors *gr_IMnpim_inter_3rd = new TGraphErrors();
+  gr_IMnpip_inter_pol1->AddPoint(anacuts::Sigmap_center, mean_Sp_pol1);
+  gr_IMnpip_inter_pol1->SetPointError(0,0.0,stddev_Sp_pol1);
+  gr_IMnpip_inter_3rd->AddPoint(anacuts::Sigmap_center+0.005, mean_Sp_3rd);
+  gr_IMnpip_inter_3rd->SetPointError(0,0.0,stddev_Sp_3rd);
+
+  gr_IMnpim_inter_pol1->AddPoint(anacuts::Sigmam_center, mean_Sm_pol1);
+  gr_IMnpim_inter_pol1->SetPointError(0,0.0,stddev_Sm_pol1);
+  gr_IMnpim_inter_3rd->AddPoint(anacuts::Sigmam_center+0.005, mean_Sm_3rd);
+  gr_IMnpim_inter_3rd->SetPointError(0,0.0,stddev_Sm_3rd);
+  
+  TCanvas *c9 = new TCanvas("c9","c9");
+  gr_IMnpip_base->SetMarkerStyle(20);
+  gr_IMnpip_base->Draw("AP");
+  gr_IMnpip_base->Print();
+  gr_IMnpip_base->RemovePoint(6);
+  Double_t *exSp;
+  Double_t *eySp;
+  exSp = gr_IMnpip_base->GetEX();
+  eySp = gr_IMnpip_base->GetEY();
+
+  for(int ip=0;ip<gr_IMnpip_base->GetN();ip++){
+    gr_IMnpip_base->SetPointError(ip,0,eySp[ip]);
+  }
+  gr_IMnpip_inter_pol1->SetMarkerStyle(20);
+  gr_IMnpip_inter_pol1->SetMarkerColor(2);
+  gr_IMnpip_inter_pol1->SetLineColor(2);
+  gr_IMnpip_inter_pol1->SetLineWidth(3);
+  gr_IMnpip_inter_pol1->Draw("p"); 
+  gr_IMnpip_inter_3rd->SetMarkerStyle(21);
+  gr_IMnpip_inter_3rd->SetMarkerColor(3);
+  gr_IMnpip_inter_3rd->SetLineColor(3);
+  gr_IMnpip_inter_3rd->SetLineWidth(3);
+  gr_IMnpip_inter_3rd->Draw("p"); 
+
+  TCanvas *c10 = new TCanvas("c10","c10");
+  gr_IMnpim_base->SetMarkerStyle(20);
+  gr_IMnpim_base->Draw("AP");
+  gr_IMnpim_base->Print();
+  gr_IMnpim_base->RemovePoint(7);
+  const int nSm = gr_IMnpim_base->GetN();
+  Double_t *exSm;
+  Double_t *eySm;
+  exSm = gr_IMnpim_base->GetEX();
+  eySm = gr_IMnpim_base->GetEY();
+
+  for(int ip=0;ip<gr_IMnpim_base->GetN();ip++){
+    gr_IMnpim_base->SetPointError(ip,0,eySm[ip]);
+  }
+  gr_IMnpim_inter_pol1->SetMarkerStyle(20);
+  gr_IMnpim_inter_pol1->SetMarkerColor(2);
+  gr_IMnpim_inter_pol1->SetLineColor(2);
+  gr_IMnpim_inter_pol1->SetLineWidth(3);
+  gr_IMnpim_inter_pol1->Draw("p"); 
+  gr_IMnpim_inter_3rd->SetMarkerStyle(21);
+  gr_IMnpim_inter_3rd->SetMarkerColor(3);
+  gr_IMnpim_inter_3rd->SetLineColor(3);
+  gr_IMnpim_inter_3rd->SetLineWidth(3);
+  gr_IMnpim_inter_3rd->Draw("p"); 
+
+
 }
