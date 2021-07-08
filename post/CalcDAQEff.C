500b36ed08230e1223f3af725eb0f9ce45a0d173
ASANO Hidemitsu <hidemitsu.asano@riken.jp>
2021-07-08 17:59:44 +0900

    fix ch. assign

--- post/CalcDAQEff.C
+++ post/CalcDAQEff.C
@@ -1,18 +1,16 @@
 void CalcDAQEff()
 {
-
-
   TFile *_file0 = TFile::Open("evanaIMpisigma_v202.root");
   TH1D* Scaler = (TH1D*)_file0->Get("Scaler");
   TCanvas *c1 = new TCanvas("c1","c1");
   Scaler->Draw("HIST");
-  const int ScCH_daqacc = 5;
-  const int ScCH_daqrej = 6;
-  double daqacc = Scaler->GetBinContent(ScCH_daqacc);
-  double daqrej = Scaler->GetBinContent(ScCH_daqrej);
-  std::cout << "DAQ acc. " << daqacc<< std::endl;
-  std::cout << "DAQ rej. " << daqrej << std::endl;
-  std::cout << "DAQ eff. " << daqacc/(daqrej+daqacc) << std::endl;
+  const int ScCH_firstTrig = 4;
+  const int ScCH_firstCompVeto = 5;
+  double firstTrig = Scaler->GetBinContent(ScCH_firstTrig);
+  double firstCompVeto = Scaler->GetBinContent(ScCH_firstCompVeto);
+  std::cout << "DAQ acc. " << firstTrig<< std::endl;
+  std::cout << "DAQ req. " << firstCompVeto << std::endl;
+  std::cout << "DAQ eff. " << (float) (firstCompVeto)/firstTrig << std::endl;
 
 
   
@@ -24,9 +22,7 @@ void CalcDAQEff()
   int iline=0;
   while(getline(ifs,str)){
     runnum[iline] = atoi(str.c_str());
-    std::cout << runnum[iline] << std::endl;
-
-
+    //std::cout << runnum[iline] << std::endl;
     iline++;
   }
   
@@ -38,13 +34,13 @@ void CalcDAQEff()
   for(int irun=0;irun<iline;irun++){
     TFile *_file = TFile::Open(Form("/gpfs/group/had/knucl/e15/asano/Run78/IMpisigmav207/evanaIMpisigma_0%03d.root",runnum[irun]),"READ");
     TH1D* hscaler = (TH1D*)_file->Get("Scaler");
-    double daqacc = hscaler->GetBinContent(ScCH_daqacc);
-    double daqrej = hscaler->GetBinContent(ScCH_daqrej);
-    hacc->SetBinContent(runnum[irun],daqacc);
-    hacc->SetBinError(runnum[irun],sqrt(daqacc));
-    hreq->SetBinContent(runnum[irun],daqacc+daqrej);
-    hreq->SetBinError(runnum[irun],sqrt(daqacc+daqrej));
-    //gdaqeff->SetPoint(gdaqeff->GetN(),runnum[irun],daqacc/(daqrej+daqacc)) ;
+    double firstTrig = hscaler->GetBinContent(ScCH_firstTrig);
+    double firstCompVeto = hscaler->GetBinContent(ScCH_firstCompVeto);
+    hreq->SetBinContent(runnum[irun],firstTrig);
+    hreq->SetBinError(runnum[irun],sqrt(firstTrig));
+    hacc->SetBinContent(runnum[irun],firstCompVeto);
+    hacc->SetBinError(runnum[irun],sqrt(firstCompVeto));
+    //gdaqeff->SetPoint(gdaqeff->GetN(),runnum[irun],firstTrig/(firstCompVeto+firstTrig)) ;
 
     _file->Close();
   }
