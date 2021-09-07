c05b1a2d47dd2f5ca2e4b93aabd256541b2f3eee
ASANO Hidemitsu <hidemitsu.asano@riken.jp>
2021-09-07 10:19:14 +0900

    update

---
+++ goodrunlist/test_kwsk.C
@@ -0,0 +1,51 @@
+void test_kwsk()
+
+{
+  for(int run=100;run<813;run++){
+
+   if(100<=run && run<=102)continue;
+    if(106<=run && run<=123)continue;
+    if(133<=run && run<=147)continue;
+    if(160<=run && run<=187)continue;
+    if(193<=run && run<=193)continue;
+    if(203<=run && run<=216)continue;
+    if(272<=run && run<=306)continue;
+    if(318<=run && run<=333)continue;
+    if(361<=run && run<=361)continue;
+    if(381<=run && run<=382)continue;
+    if(405<=run && run<=405)continue;
+    if(434<=run && run<=453)continue;
+    if(489<=run && run<=514)continue;
+    if(561<=run && run<=577)continue;
+    if(618<=run && run<=622)continue;
+    if(625<=run && run<=625)continue;
+    if(628<=run && run<=629)continue;
+    if(654<=run && run<=673)continue;
+    if(721<=run && run<=723)continue;
+    if(741<=run && run<=755)continue;
+    if(759<=run && run<=761)continue;
+    if(799<=run && run<=800)continue;
+
+    //v4
+    if(run==103)continue;
+    if(run==124)continue;
+    if(run==404)continue;
+    if(run==635)continue;
+    if(run==636)continue;
+    if(run==643)continue;
+
+    std::cout << run << std::endl;
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+}
