c9f65346120a85692718fa83df90e5ff079e60d6
ASANO Hidemitsu <hidemitsu.asano@riken.jp>
2021-10-13 17:00:24 +0900

    update

--- simpost/GetAccMapLpim.C
+++ simpost/GetAccMapLpim.C
@@ -12,13 +12,13 @@ void GetAccMapLpim()
   TFile *fLpim=NULL;
   TFile *fgen=NULL;
 
-  fLpim = TFile::Open("simIMLpim_ppimpim_v18_out.root");
-  fgen = TFile::Open("simIMLpim_v18.root");
+  fLpim = TFile::Open("simIMLpim_ppimpim_v20_out.root");
+  fgen = TFile::Open("simIMLpim_v20.root");
   TFile *fkin = TFile::Open("NumericalRootFinderLPim.root","READ");
 //  fLpim = TFile::Open("simIMLpim_ppimpim_pS0pim_v1_out.root");
 //  fgen = TFile::Open("simIMLpim_pS0pim_v1.root");
   
-  TH2F* q_IMLPim_gen;
+  TH2F* q_IMLPim_gen=NULL;
   q_IMLPim_gen = (TH2F*) fgen->Get("React_q_IMLPim");
 //  q_IMLPim_gen = (TH2F*) fgen->Get("React_q_IMS0Pim");
   q_IMLPim_gen->SetTitle("generated evt. ");
@@ -31,6 +31,7 @@ void GetAccMapLpim()
   q_IMLPim_gen->RebinY(3);
   q_IMLPim_gen->Print("base");
   
+
    
   TH2F* q_IMppipi_p_wL_sum;
   q_IMppipi_p_wL_sum= (TH2F*)fLpim->Get("q_IMppipi_p_wL_sum");
@@ -103,11 +104,133 @@ void GetAccMapLpim()
   cLpim->cd(4);
   q_IMppipi_p_wL_accerr->SetMaximum(0.5);
   q_IMppipi_p_wL_accerr->Draw("colz");
-    
+   
+  //test for reco.events/acc map 
+  TCanvas *cLpim_gentest = new TCanvas("cLpim_gentest","cLpim_gentest",2000,800);
+  cLpim_gentest->Divide(2,1);
+  cLpim_gentest->cd(1);
+  TH2F* q_IMppipi_p_wL_gentest = (TH2F*)q_IMppipi_p_wL_sum->Clone("q_IMppipi_p_wL_gentest");
+  q_IMppipi_p_wL_gentest->Divide(q_IMppipi_p_wL_acc);
+  q_IMppipi_p_wL_gentest->SetTitle("TEST reco/acc map");
+  q_IMppipi_p_wL_gentest->Draw("colz");
+  cLpim_gentest->cd(2);
+  q_IMLPim_gen->Draw("colz");
+   
+  TH1F* htest = new TH1F("htest","htest",14900,100,15000);
+  for(int ix=0;ix<q_IMppipi_p_wL_gentest->GetNbinsX();ix++){
+    for(int iy=0;iy<q_IMppipi_p_wL_gentest->GetNbinsY();iy++){
+      double cont = q_IMppipi_p_wL_gentest->GetBinContent(ix,iy);
+      htest->Fill(cont);
+    }
+  }
+  TCanvas *ctest  = new TCanvas("ctest","ctest",1000,800);
+  htest->Draw();
+
+  TCanvas *ccleanup = new TCanvas("ccleanup","ccleanup",1000,800);
+  TH2F* q_IMppipi_p_wL_gentest_clean = (TH2F*)q_IMppipi_p_wL_gentest->Clone("q_IMppipi_p_wL_gentest_clean");
+  TH2F* q_IMppipi_p_wL_acc_clean = (TH2F*)q_IMppipi_p_wL_acc->Clone("q_IMppipi_p_wL_acc_clean");
+  for(int ix=0;ix<q_IMppipi_p_wL_gentest->GetNbinsX();ix++){
+    for(int iy=0;iy<q_IMppipi_p_wL_gentest->GetNbinsY();iy++){
+      double cont = q_IMppipi_p_wL_gentest->GetBinContent(ix,iy);
+      if(cont < 11580.-5.*169.5 || 11580.+5.*169.5<cont){
+        q_IMppipi_p_wL_gentest_clean->SetBinContent(ix,iy,0);
+        q_IMppipi_p_wL_gentest_clean->SetBinError(ix,iy,0);
+        q_IMppipi_p_wL_acc_clean->SetBinContent(ix,iy,0);
+        q_IMppipi_p_wL_acc_clean->SetBinError(ix,iy,0);
+      }
+    }
+  }
+  q_IMppipi_p_wL_gentest_clean->SetMaximum(q_IMppipi_p_wL_gentest->GetMaximum());
+  q_IMppipi_p_wL_gentest_clean->Draw("colz");
+  
+
+  TCanvas *cacccleanup = new TCanvas("cacccleanup","cacccleanup",1000,800);
+  q_IMppipi_p_wL_acc_clean->SetMaximum(q_IMppipi_p_wL_acc->GetMaximum());
+  q_IMppipi_p_wL_acc_clean->Draw("colz");
+
+  //////////////////////////////////////////////////////////
+  //acceptance map for costheta_missing p v.s. IM(pi-Lambda) 
+  //////////////////////////////////////////////////////////
+  TH2F* costhetap_IMLpim_gen=NULL;
+  costhetap_IMLpim_gen = (TH2F*)fgen->Get("React_costhetap_IMLPim");
+  costhetap_IMLpim_gen->SetTitle("generated evt. ");
+  costhetap_IMLpim_gen->SetXTitle("true IM(#pi^{-}#Lambda) [GeV/c^{2}]");
+  costhetap_IMLpim_gen->SetYTitle("true proton CosTheta");
+  costhetap_IMLpim_gen->GetXaxis()->CenterTitle();
+  costhetap_IMLpim_gen->GetYaxis()->CenterTitle();
+  costhetap_IMLpim_gen->Print("base");
+  costhetap_IMLpim_gen->RebinX(15);
+  costhetap_IMLpim_gen->Print("base");
  
-//  TFile *fout = new TFile("accmapLpim_pS0pim.root","RECREATE");
+  TH2F* CosTheta_IMppipi_p_wL_sum=NULL;
+  CosTheta_IMppipi_p_wL_sum = (TH2F*)fLpim->Get("CosTheta_IMppipi_p_wL_sum");
+  CosTheta_IMppipi_p_wL_sum ->SetTitle("reco. evt.");
+
+  TH2F* CosTheta_IMppipi_p_wL_acc=NULL;
+  CosTheta_IMppipi_p_wL_acc = (TH2F*)CosTheta_IMppipi_p_wL_sum->Clone("CosTheta_IMppipi_p_wL_acc");
+  CosTheta_IMppipi_p_wL_acc->SetTitle("CosTheta_IMppipi_p_wL_acc");
+  CosTheta_IMppipi_p_wL_acc->Print("base");
+  CosTheta_IMppipi_p_wL_acc->Divide(CosTheta_IMppipi_p_wL_acc,costhetap_IMLpim_gen,1.0,1.0,"b");
+
+
+  TH2F* CosTheta_IMppipi_p_wL_accerr=NULL;
+  CosTheta_IMppipi_p_wL_accerr = (TH2F*)CosTheta_IMppipi_p_wL_acc->Clone("CosTheta_IMppipi_p_wL_accerr");
+  CosTheta_IMppipi_p_wL_accerr->Reset();
+  CosTheta_IMppipi_p_wL_accerr->SetTitle("CosTheta_IMppipi_p_wL_acc precision");
+  
+  for(int ix=0;ix<CosTheta_IMppipi_p_wL_accerr->GetNbinsX();ix++){
+    for(int iy=0;iy<CosTheta_IMppipi_p_wL_accerr->GetNbinsY();iy++){
+      double cont = CosTheta_IMppipi_p_wL_acc->GetBinContent(ix,iy);
+      double err = CosTheta_IMppipi_p_wL_acc->GetBinError(ix,iy);
+      if(cont!=0){
+        CosTheta_IMppipi_p_wL_accerr->SetBinContent(ix,iy,err/cont);  
+      }
+    }
+  }
+  
+
+
+  TCanvas *cLpimCos;
+  cLpimCos = new TCanvas(Form("cLpimCos"),Form("cLpimCos"),2000,1200);
+  cLpimCos->Divide(2,2);
+  cLpimCos->cd(1);
+  costhetap_IMLpim_gen->GetYaxis()->SetRangeUser(0,1);
+  costhetap_IMLpim_gen->Draw("colz");
+  gPad->SetLogz();
+  cLpimCos->cd(2);
+  CosTheta_IMppipi_p_wL_sum->GetYaxis()->SetRangeUser(0,1);
+  CosTheta_IMppipi_p_wL_sum->Draw("colz");
+  gPad->SetLogz();
+  cLpimCos->cd(3);
+  //CosThetaIMppipi_p_wL_acc->SetMaximum(0.05);
+  CosTheta_IMppipi_p_wL_acc->GetYaxis()->SetRangeUser(0,1);
+  CosTheta_IMppipi_p_wL_acc->Draw("colz");
+  gPad->SetLogz();
+  cLpimCos->cd(4);
+  //CosThetaIMppipi_p_wL_accerr->SetMaximum(0.5);
+  CosTheta_IMppipi_p_wL_accerr->GetYaxis()->SetRangeUser(0,1);
+  CosTheta_IMppipi_p_wL_accerr->Draw("colz");
+  
+  TCanvas *cLpimCosTest = new TCanvas("cLpimCosTest","cLpimCosTest",1000,800);
+  TH2F* CosTheta_IMppipi_p_wL_test = (TH2F*)CosTheta_IMppipi_p_wL_sum->Clone("CosTheta_IMppipi_p_wL_test");
+  CosTheta_IMppipi_p_wL_test->Divide(CosTheta_IMppipi_p_wL_acc);
+  CosTheta_IMppipi_p_wL_test->Draw("colz");
+
+  TCanvas *cLpimCosTestDiff = new TCanvas("cLpimCosTestDiff","cLpimCosTestDiff",1000,800);
+  TH1D* hLpimCosTestDiff = new TH1D("hLpimCosTestDiff","hLpimCosTestDiff",10000,-5000,5000);
+  for(int ix=0;ix<CosTheta_IMppipi_p_wL_test->GetNbinsX();ix++){
+    for(int iy=0;iy<CosTheta_IMppipi_p_wL_test->GetNbinsY();iy++){
+      double conttest = CosTheta_IMppipi_p_wL_test->GetBinContent(ix,iy);
+      double contgen = costhetap_IMLpim_gen->GetBinContent(ix,iy);
+      hLpimCosTestDiff->Fill(conttest-contgen);
+    }
+  }
+  hLpimCosTestDiff->Draw();
+
+  //  TFile *fout = new TFile("accmapLpim_pS0pim.root","RECREATE");
   TFile *fout = new TFile("accmapLpimv18.root","RECREATE");
   q_IMppipi_p_wL_acc->Write();
   q_IMppipi_p_wL_accerr->Write();
+  q_IMppipi_p_wL_acc_clean->Write();
   fout->Close();
 }
