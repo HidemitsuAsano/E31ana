ae1a814e6f6bdb133ad8aa0a88dab278615b5033
ASANO Hidemitsu <hidemitsu.asano@riken.jp>
2021-12-13 18:22:11 +0900

    update

--- post/FitCslpim.C
+++ post/FitCslpim.C
@@ -24,6 +24,31 @@ Double_t BWandFormP(Double_t *x,Double_t *par)
 
 }
 
+//Fit with BW and some phenomenological model
+Double_t BWandLandau(Double_t *x,Double_t *par)
+{
+  //BW and scaling factor
+  //Double_t BW = par[0]*pow(par[1]/2.0,2.0)/((pow(x[0]-par[2]),2.0)+pow(par[1]/2.0,2.0));
+  Double_t BW = TMath::BreitWigner(x[0],par[2],par[1]);
+  //Phenomenological model
+  Double_t Ph= TMath::Landau(x[1],par[3],par[4]);
+
+
+  return par[0]*BW*Ph;
+
+}
+
+//Voigt and Landau
+Double_t VGandLandau(Double_t *x,Double_t *par)
+{
+  Double_t VG = TMath::Voigt(x[0]-par[2],par[5],par[1],4);
+  //Phenomenological model
+  Double_t Ph= TMath::Landau(x[1],par[3],par[4]);
+  
+  return par[0]*VG*Ph;
+
+}
+
 
 
 void FitCslpim()
@@ -74,14 +99,77 @@ void FitCslpim()
   }
   CS_sum_fit->Draw("colz");
   
-  TF2 *f2 = new TF2("f2",BWandFormP,1.30,1.40,0.4,0.8,5);
-  f2->SetParameter(1,0.1);
-  f2->FixParameter(2,1.38);
-  f2->SetRange(1.30,1.40,0.4,0.8);
+  //TF2 *f2 = new TF2("f2",BWandFormP,1.30,1.40,0.4,0.8,5);
+  //f2->SetParameter(1,0.1);
+  //f2->FixParameter(2,1.38);
+  //f2->SetRange(1.30,1.40,0.4,0.8);
+  //CS_sum_fit->Fit("f2","R","");
+  //f2->Draw("cont1 same");
+  
+  TCanvas *cfittest = new TCanvas("cfittest","cfittest");
+  cfittest->Divide(2,2);
+  cfittest->cd(3);
+  
+  /*
+  TF2 *f2 = new TF2("f2",BWandLandau,1.32,1.45,0.4,0.75,5);
+  f2->SetParLimits(0,0,10000);
+  f2->SetParameter(0,0.0001);
+  f2->SetParameter(1,0.045);
+  f2->FixParameter(2,1.3872);//PDG mass
+  f2->SetParameter(3,0.47);
+  f2->SetParLimits(3,0.2,0.5);
+  f2->SetParameter(4,0.11);
+  f2->SetParLimits(4,0.1,1000000);
+  f2->SetNpx(100);
+  f2->SetNpy(100);
+  //CS_sum_fit->Fit("f2","R","");
+  //f2->Draw("cont1 same");
+  f2->Print("base");
+  //f2->Draw("cont1 ");
+  TH2D* f2hist = (TH2D*)f2->GetHistogram();
+  f2hist->SetLineColor(2);
+  f2hist->Draw("colz");
+  */
+
+  TF2 *f2 = new TF2("f2",VGandLandau,1.32,1.44,0.39,0.75,6);
+  f2->SetParLimits(0,0,10000);
+  f2->SetParameter(0,0.00025);
+  f2->SetParameter(1,0.045);
+  f2->FixParameter(2,1.3872);//PDG mass
+  f2->SetParameter(3,0.47);
+  f2->SetParLimits(3,0.2,0.5);
+  f2->SetParameter(4,0.11);
+  f2->SetParLimits(4,0.1,1000000);
+  f2->FixParameter(5,0.001);
+  f2->SetNpx(8);
+  f2->SetNpy(12);
+  f2->Print("base");
   CS_sum_fit->Fit("f2","R","");
-  f2->Draw("cont1 same");
+  //f2->Draw("cont1 ");
 
+  TH2D* f2hist = (TH2D*)f2->GetHistogram();
+  f2hist->SetLineColor(2);
+  f2hist->Draw("colz");
+  CS_sum_fit->Print("base");
+  f2hist->Print("base");
 
+  cfittest->cd(1);
+  const int binx1320 = CS_sum_fit->GetXaxis()->FindBin(1.32);
+  const int binx1440 = CS_sum_fit->GetXaxis()->FindBin(1.44);
+  const int biny400 = CS_sum_fit->GetYaxis()->FindBin(0.39);
+  const int biny750 = CS_sum_fit->GetYaxis()->FindBin(0.75);
+  CS_sum_fit->ProjectionX("CS_sum_fit_px",biny400,biny750)->Draw("HE");
+  f2hist->SetFillColor(0);
+  TH1D* f2hist_px = (TH1D*)f2hist->ProjectionX("f2hist_px");
+  std::cout << "width x " << f2hist_px->GetBinWidth(1) << std::endl;
+  //f2hist_px->Rebin(5);
+  f2hist_px->Draw("same");
+
+  cfittest->cd(4);
+  CS_sum_fit->ProjectionY("CS_sum_fit_py",binx1320,binx1440)->Draw("HE");
+  TH1D* f2hist_py = (TH1D*)f2hist->ProjectionY();
+  f2hist_py->Draw("same");
+  std::cout << "width y " << f2hist_py->GetBinWidth(1) << std::endl;
   TCanvas *cCS_q_fit = new TCanvas("cCS_q_fit","cCS_q_fit",1000,800);
   
   const int bin1360 = CS_sum_fit->GetXaxis()->FindBin(1.36);
@@ -91,6 +179,38 @@ void FitCslpim()
   CS_q_fit->Draw("E");
   TF1 *f1 = new TF1("f1",FormP,0.4,0.8,2);
   CS_q_fit->Fit("f1","","",0.4,0.75);
+
+
+  TCanvas *c = NULL;
+  TSeqCollection *SCol = gROOT->GetListOfCanvases();
+  int size = SCol->GetSize();
+  TIter next(SCol);
+  TString pdfname = "fitlpim.pdf";
+  for(int i=0;i<size;i++){
+    //pdf->NewPage();
+    c= (TCanvas*)next();
+    c->Modified();
+    c->Update();
+    c->Draw();
+    c->cd();
+    //inside the canvas
+    //TPaveText *pt = new TPaveText(.74,.81,0.9,0.90,"NDC");
+    c->Modified();
+    c->Update();
+    c->GetListOfPrimitives();//->Print();
+    //std::cout << c->GetName() << std::endl;
+    //make 1 pdf file
+    if(i==0) c->Print(pdfname+"(",Form("pdf Title:%s",c->GetTitle()));
+    else if(i==size-1)c->Print(pdfname+")",Form("pdf Title:%s",c->GetTitle())); 
+    else c->Print(pdfname,Form("pdf Title:%s",c->GetTitle())); 
+    //make separated pdf files
+    //c->Print(Form("pdf/%s.pdf",c->GetTitle()));
+  }
+
+
+
+
+
 
   return;
 
