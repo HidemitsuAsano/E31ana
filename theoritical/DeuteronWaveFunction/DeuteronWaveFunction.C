23522ccd5b42bccbadc9311917febb58dae6465c
hide <hide@DESKTOP-BJNR34K.localdomain>
2022-11-14 11:08:31 +0900

    update

--- theoritical/DeuteronWaveFunction/DeuteronWaveFunction.C
+++ theoritical/DeuteronWaveFunction/DeuteronWaveFunction.C
@@ -4,26 +4,30 @@
 
 const double hbarc=0.1973269804; // GeV*fm
 
+//C(5)
 const double gammaMass=0.2315380; // fm^(-1)
 const double m0=0.9; // fm^(-1)
-const double C[]={ 0.88472985, -0.26408759, -0.44114404e-1, -0.14397512e2, 0.85591256e2, -0.31876761e3, 0.70336701e3, -0.90049586e3, 0.66145441e3, -0.25958894e3 };
+const double C_swave[]={ 0.88472985, -0.26408759, -0.44114404e-1, -0.14397512e2, 0.85591256e2, -0.31876761e3, 0.70336701e3, -0.90049586e3, 0.66145441e3, -0.25958894e3 };
+const double C_dwave[]={                                                };
+
 
 void DeuteronWaveFunction(){
-  double sum=0;
+  double sum_C_swave=0;
   std::vector<double> conv_C;
-  for( int i=0; i<sizeof(C)/sizeof(C[0]); i++ ){
-    std::cout<<"C_"<<i<<"="<<C[i]<<std::endl;
-    conv_C.push_back(C[i]);
-    sum+=C[i];
+  for( int i=0; i<sizeof(C_swave)/sizeof(C_swave[0]); i++ ){
+    std::cout<<"C_"<<i<<"="<<C_swave[i]<<std::endl;
+    conv_C.push_back(C_swave[i]);
+    sum_C_swave+=C_swave[i];
   }
-  std::cout<<-sum<<std::endl;
-  conv_C.push_back(-sum);
+  std::cout<<-sum_C_swave<<std::endl;
+  conv_C.push_back(-sum_C_swave);
 
-  sum=0;
+  sum_C_swave=0;
   for( int i=0; i<conv_C.size(); i++ ){
-    sum+=conv_C[i];
+    sum_C_swave+=conv_C[i];
   }
-  std::cout<<sum<<std::endl;
+  //check sum should be 0
+  std::cout<<sum_C_swave<<std::endl;
 
   std::vector<double> x, y;
   //  for( double p=0.1; p<1.0; p+=0.1 ){
